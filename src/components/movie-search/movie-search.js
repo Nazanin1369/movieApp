fd47306c9e02e37e592756b5891a09b9c3af679a
Nazanin Delam <ndelam@myvest.com>
2018-12-01 19:01:52 -0800

    search functionality

--- src/components/movie-search/movie-search.js
+++ src/components/movie-search/movie-search.js
@@ -11,22 +11,55 @@ class MovieSearch extends HTMLElement {
         link.href = 'src/components/movie-search/movie-search.css';
 
         let section = document.createElement('section');
-        section.setAttribute('class', 'search');
-
         let searchContainer = document.createElement('div');
-        searchContainer.setAttribute('class', 'search__container');
-
         let searchInput = document.createElement('input');
+        let searchResults = document.createElement('datalist');
+        let result1 = document.createElement('option');
+        let result2 = document.createElement('option');
+
+        section.setAttribute('class', 'search');
+        searchContainer.setAttribute('class', 'search__container');
         searchInput.setAttribute('class', 'search__input');
         searchInput.setAttribute('type', 'text');
         searchInput.setAttribute('placeholder', 'Search movies');
         searchInput.setAttribute('list', 'movies');
+        searchResults.setAttribute('id', 'movies');
+        result1.setAttribute('value', 'res1');
+        result2.setAttribute('value', 'res2');
 
+        searchResults.appendChild(result1);
+        searchResults.appendChild(result2);
         searchContainer.appendChild(searchInput);
+        searchContainer.appendChild(searchResults);
         section.appendChild(searchContainer);
 
         this.shadowRoot.appendChild(link);
         this.shadowRoot.appendChild(section);
+
+        this.search('hello');
+    }
+
+    search(title) {
+        fetch(`http://www.omdbapi.com/?apikey=aba065d3&s=${title}`)
+        .then(response => response.json())
+        .then(data => {
+           return this.buildDataList(data.Search);
+        })
+        .catch(error => {
+            console.log('There has been a problem with your fetch operation: ', error.message);
+        });
+    }
+
+    buildDataList(items) {
+        let documentFragmentEl = document.createDocumentFragment();
+        for(let item of items) {
+            console.log(item);
+            let itemEl = document.createElement('option');
+            itemEl.setAttribute('value', item.Title);
+            documentFragmentEl.appendChild(itemEl);
+        }
+
+        this.shadowRoot.getElementById('movies').appendChild(documentFragmentEl);
     }
 }
 
